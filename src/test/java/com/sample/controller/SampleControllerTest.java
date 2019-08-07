bf68cd665212f6198ccbe01f9685f630e3425e22
Arnaud Combeau <arnaud.combeau@osb.group>
2019-08-07 02:02:44 +0200

    init test

---
+++ src/test/java/com/sample/controller/SampleControllerTest.java
@@ -0,0 +1,29 @@
+package com.sample.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(SampleControllerTest.class)
+public class SampleControllerTest {
+
+  @Autowired private MockMvc mockMvc;
+
+  @Test
+  public void test1() throws Exception {
+    assertEquals(true)
+  
+  
+}
